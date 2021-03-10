b2a5c7dcb23c1fc3b6c461c585f417f00b4c6dea
Marcos Piemontez <marcos.piemontez1@hotmail.com>
2021-03-10 14:56:47 -0300

    Atualização

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,12 @@
 <template>
-  <!-- o q-page é obrigatório quando há paginas! -->
   <q-page class="conteiner" padding>
-    <!-- foi criado um texto em 'formato' de título -->
     <p class="text-h4 text-center">Formulário</p>
-    <!-- para criação de formulários é sempre interessante ter essa tag 'q-form' -->
     <q-form
       @submit="onSubmit"
       @reset="onReset"
       ref="myForm"
       class="row q-gutter-md flex flex-center"
     >
-      <!-- o @submit e o @reset está ligando à lógica JS feita la em baixo, o 'ref="myForm"' foi feito para referenciar ao clear do formulário ao concluir. -->
       <q-input
         outlined
         color="deep-purple"
@@ -19,14 +15,10 @@
         class="col-md-6 col-sm-12 col-xs-12"
         :rules="[val => (val && val.length > 0) || 'Nome obrigatório']"
       >
-        <!-- o input de cima foi feito para o nome da pessoa, v-model=form.nome liga ao JS la em baixo, :rules é a lógica implantada -->
-        <!-- o template abaixo clia um icone na linha do input, sendo o v-slot:prepend aplica o icone no inicio do input,  -->
         <template v-slot:prepend>
           <q-icon name="person" />
         </template>
       </q-input>
-      <!-- input do botão de idade, o :rules traz a lógica que "valor recebe valor diferente de null e diferente de vazio
-      valor recebe valor e valor tem q ser maior que 0 e menor q 100" -->
       <q-input
         outlined
         color="deep-purple"
@@ -43,7 +35,6 @@
           <q-icon name="person" />
         </template>
       </q-input>
-      <!-- rules: valor recebe valor e valor tem q ter o comprimento maios que 0 -->
       <q-input
         outlined
         color="deep-purple"
@@ -57,8 +48,6 @@
           <q-icon name="mail" />
         </template>
       </q-input>
-      <!-- rules, valor recebe valor e valor tem q ser maior que 0,
-      rules valor recebe valor e e tem q ser igual a 11 digitos -->
       <q-input
         outlined
         color="deep-purple"
@@ -73,7 +62,6 @@
         ]"
       >
         <template v-slot:prepend>
-          <!-- esse prepende faz com que o icone fique no inicio do input, p/ por no final do input seria "append" -->
           <q-icon name="call" />
         </template>
       </q-input>
